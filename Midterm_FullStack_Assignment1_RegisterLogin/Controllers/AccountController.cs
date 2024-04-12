d2b3a3d970c44df9ee68fb3bc2e6b99bbf43e770
GarciaMarkAngelo-BSIT32E1 <160812645+GarciaMarkAngelo-BSIT32E1@users.noreply.github.com>
2024-04-12 21:02:11 +0800

    Created A controller that will handle User registration.

---
+++ Midterm_FullStack_Assignment1_RegisterLogin/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Midterm_FullStack_Assignment1_RegisterLogin.Models;
+using System.Linq;
+
+public class AccountController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public AccountController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(User user)
+    {
+        if (ModelState.IsValid)
+        {
+            // Check if the username already exists
+            var existingUser = _context.Users.FirstOrDefault(u => u.Username == user.Username);
+            if (existingUser != null)
+            {
+                ModelState.AddModelError("", "Username already exists. Please choose a different one.");
+                return View(user);
+            }
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return RedirectToAction("Login");
+        }
+        return View(user);
+    }
+}
