b7c191b678879ca70ef412896ad503a9d40411fe
LEONEL JERRY LEGARA <1176-21@itmlyceumalabang.onmicrosoft.com>
2024-04-12 20:33:49 +0800

    created a database context

---
+++ Midterm_FullStack_Assignment1_RegisterLogin/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Midterm_FullStack_Assignment1_RegisterLogin.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
