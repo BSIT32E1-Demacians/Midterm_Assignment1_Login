f30c49b926c9ca2686ddd1711c3075bfa4a8836a
Gabriel <gabrielpabalesalazar@gmail.com>
2024-04-12 19:35:03 +0800

    Added a Model for User

---
+++ Midterm_FullStack_Assignment1_RegisterLogin/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Midterm_FullStack_Assignment1_RegisterLogin.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
