7b66ecd157ef8bb7535aacfd6caf3e924c0eee8f
Rodejo_VaronIan_BSIT32E1 <varonianrodejo@gmail.com>
2024-04-19 20:42:17 +0800

    Added AppDbContext to Domain Layer

--- Midterm_FullStack_Assignment1_RegisterLogin/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Midterm_FullStack_Assignment1_RegisterLogin.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
