caac43a7eaeeab39e10d3360e378e08bec631c28
smarty0917 <martintristanmreyes@gmail.com>
2024-04-20 12:16:51 +0800

    added repository interface

---
+++ Repository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+
+namespace Repository
+{
+    public interface IUserRepository
+    {
+        User Create(User user);
+        User GetById(int id);
+        User GetByUsername(string username);
+        IEnumerable<User> GetAll();
+    }
+}
